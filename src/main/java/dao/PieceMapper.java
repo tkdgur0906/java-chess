f6d35aeec2e1e7574b6a1a77df8a3f6429032a95
KimSangHyeok <tkdgur0906@naver.com>
2024-03-31 22:58:29 +0900

    refactor: PieceMapper에 asString 활용해 불필요 필드 제거

--- src/main/java/dao/PieceMapper.java
+++ src/main/java/dao/PieceMapper.java
@@ -14,35 +14,31 @@ import java.util.Arrays;
 
 public enum PieceMapper {
 
-    WHITE_KING("k", new King(Color.WHITE)),
-    WHITE_QUEEN("q", new Queen(Color.WHITE)),
-    WHITE_ROOK("r", new Rook(Color.WHITE)),
-    WHITE_KNIGHT("n", new Knight(Color.WHITE)),
-    WHITE_BISHOP("b", new Bishop(Color.WHITE)),
-    WHITE_PAWN("p", new Pawn(Color.WHITE)),
-    BLACK_KING("K", new King(Color.BLACK)),
-    BLACK_QUEEN("Q", new Queen(Color.BLACK)),
-    BLACK_ROOK("R", new Rook(Color.BLACK)),
-    BLACK_KNIGHT("N", new Knight(Color.BLACK)),
-    BLACK_BISHOP("B", new Bishop(Color.BLACK)),
-    BLACK_PAWN("P", new Pawn(Color.BLACK)),
-    EMPTY(".", new Empty());
+    WHITE_KING(new King(Color.WHITE)),
+    WHITE_QUEEN(new Queen(Color.WHITE)),
+    WHITE_ROOK(new Rook(Color.WHITE)),
+    WHITE_KNIGHT(new Knight(Color.WHITE)),
+    WHITE_BISHOP(new Bishop(Color.WHITE)),
+    WHITE_PAWN(new Pawn(Color.WHITE)),
+    BLACK_KING(new King(Color.BLACK)),
+    BLACK_QUEEN(new Queen(Color.BLACK)),
+    BLACK_ROOK(new Rook(Color.BLACK)),
+    BLACK_KNIGHT(new Knight(Color.BLACK)),
+    BLACK_BISHOP(new Bishop(Color.BLACK)),
+    BLACK_PAWN(new Pawn(Color.BLACK)),
+    EMPTY(new Empty());
 
-    private final String pieceText;
     private final Piece piece;
 
-
-    PieceMapper(String pieceText, Piece piece) {
-        this.pieceText = pieceText;
+    PieceMapper(Piece piece) {
         this.piece = piece;
     }
 
     public static Piece textToPiece(String input) {
         return Arrays.stream(values())
-                .filter(pieceMapper -> pieceMapper.pieceText.equals(input))
+                .filter(pieceMapper -> pieceMapper.piece.asString().equals(input))
                 .map(pieceMapper -> pieceMapper.piece)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 말의 모양입니다."));
-
     }
 }
